a646170d096eb7534692039926ddea7185d21b3e
PanupongThongdee <pranupong.49@gmail.com>
2025-11-21 00:01:54 +0700

    Lab1.1

---
+++ Lab1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // นำเข้าคำสั่งScanner
+
+public class Lab1 { // ห้องขนาดใหญ่
+  public static void main(String[] args) { // main 
+
+    System.out.println("Enter is sum Number :");
+    Scanner num1 =new Scanner(System.in);//รับค่าจากแป้นพิมพ์
+    Scanner num2 =new Scanner(System.in);//รับค่าจากแป้นพิมพ์
+
+    System.out.print("Enter num1 : "); 
+    int n1 =num1.nextInt();//นำค่าจากแป้นพิมพ์ มาใส่ในตัวแปร n1
+    System.out.print("Enter num2 : "); 
+    int n2 =num2.nextInt();//นำค่าจากแป้นพิมพ์ มาใส่ในตัวแปร n2
+
+    System.out.println("Answer = "+n1+n2);//แสดงผลลัพธ์
+  }
+}
