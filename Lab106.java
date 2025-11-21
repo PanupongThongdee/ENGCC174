8d5eb0affc2555329b45c958bcf4026cf3d41cbf
PanupongThongdee <pranupong.49@gmail.com>
2025-11-21 12:37:46 +0700

    Lab106

--- Lab106.java
+++ Lab106.java
@@ -10,7 +10,7 @@
 
             int num = numberMultiplication.nextInt();//สร้างตัวแปร num ที่เป็นประเภท int ไว้เก็บค่าจาก numberMultiplication ที่รับมาจากแป้นพิมพ์
 
-            for(int fristLoop = 0 ; fristLoop <=12 ; fristLoop++){ //forLoop 
+            for(int fristLoop = 1 ; fristLoop <=12 ; fristLoop++){ //forLoop 
 
                 System.out.printf("%d * %d = %d\n",num ,fristLoop,(num*fristLoop));//แสดงค่าคำตอบ
                     
