c447a36005a1ec9c7828acd5ea08f5119c80887d
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 13:32:38 +0700

    Lab106

--- Lab106.java
+++ Lab106.java
@@ -2,17 +2,13 @@
 
     public class Lab106 {
 
-        public static void main(String[] text){ 
+        public static void main(String[] soBad){ 
 
-            Scanner numberMultiplication = new Scanner(System.in); 
-
+            Scanner mul = new Scanner(System.in); 
             System.out.print("Enter Number of multiplication table :");
-
-            int num = numberMultiplication.nextInt();
-
-            for(int fristLoop = 1 ; fristLoop <=12 ; fristLoop++){ 
-
-            System.out.printf("%d * %d = %d\n",num ,fristLoop,(num*fristLoop));
+            int num = mul.nextInt();
+            for(int i = 1 ; i <=12 ; i++){ 
+                 System.out.printf("%d * %d = %d\n",num ,i,(num*i));
                     
     }
 }
