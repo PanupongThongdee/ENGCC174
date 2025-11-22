10775de489c042f167bb426e2d70943660bd4742
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 11:39:47 +0700

    Lab108

--- Lab108.java
+++ Lab108.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;//เรียกใช้คำสั่ง Scanner
 
 public class Lab108 {
-    public static void main(String[] text){ //main
-    Scanner enterNumber =new Scanner(System.in);    //สร้างตัวแปร enterNumber ไว้เก็บค่าจากคีย์บอร์ด
+    public static void main(String[] nigger){
+    Scanner enterNumber =new Scanner(System.in); 
     System.out.print("Enter Number :");
-    int number=enterNumber.nextInt(); //สร้างตัวแปร number ที่เป็นประเภท int ไว้เก็บค่าจาก entenNumber ที่รับมาจากแป้นพิมพ์
-    int[] arryNumber = new int[number]; //สร้างตัวแปร arryNumber เป็น array และกำหนดความกว้างช่อง index ด้วยตัวแปร number 
-    for(int i = 0 ;i < number ;i++ ){ // loop i  ไว้กรอกตัวเลขใส่ในแต่ละindex
-      arryNumber[i] =enterNumber.nextInt();  //กรอกตัวเลขใส่ในแต่ละ index
+    int number=enterNumber.nextInt();
+    int[] arryNumber = new int[number];
+    for(int i = 0 ;i < number ;i++ ){ 
+      arryNumber[i] =enterNumber.nextInt(); 
         
     }
     int max = 0;
@@ -23,3 +23,12 @@ public class Lab108 {
      
     }
 }
+
+
+
+
+
+
+
+
+
