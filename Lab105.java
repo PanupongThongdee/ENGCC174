bd3ceee142f22a957b180e8b18739f0e3e8c47b7
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 11:56:52 +0700

    Lab105

--- Lab105.java
+++ Lab105.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;//เรียกใช้คำสั่ง Scanner
+import java.util.Scanner;
 
- public class Lab105 { //class หลัก
-    public static void main(String[] text){ // main box
-       Scanner enterNumber =new Scanner(System.in); //สร้างตัวแปร EnterNumber ไว้เก็บค่าจากคีย์บอร์ด
+ public class Lab105 { 
+    public static void main(String[] text){ 
+       Scanner enterNumber =new Scanner(System.in); 
        System.out.print("Enter Menu Number : ");
-       int menu = enterNumber.nextInt(); //สร้างตัวแปร menu ที่เป็นประเภท int ไว้เก็บค่าจาก EnterNumber ที่รับมาจากแป้นพิมพ์
+       int menu = enterNumber.nextInt(); 
 
-       if(menu == 1){ //นำค่าจากตัวแปรmenuมาเช็ค ถ้าตรวจสอบได้ 1 ก็จะแสดงผลข้อความ
+       if(menu == 1){ 
           System.out.println("Americano");
        }
-       else if(menu == 2){//นำค่าจากตัวแปรmenuมาเช็ค ถ้าตรวจสอบได้ 2 ก็จะแสดงผลข้อความ
+       else if(menu == 2){
           System.out.println("Latte");
        }
-       else if(menu == 3){//นำค่าจากตัวแปรmenuมาเช็ค ถ้าตรวจสอบได้ 3 ก็จะแสดงผลข้อความ
+       else if(menu == 3){
           System.out.println("Espresso");
        }
-       else if(menu == 4){//นำค่าจากตัวแปรmenuมาเช็ค ถ้าตรวจสอบได้ 4 ก็จะแสดงผลข้อความ
+       else if(menu == 4){
           System.out.println("Mocha");
        }
-       else{//นำค่าจากตัวแปรmenuมาเช็ค ถ้าตรวจสอบได้นอกเหนือจากเงื่อนไขข้างบน ก็จะแสดงผลข้อความ "Invalid Menu"
+       else{
           System.out.println("Invalid Menu");
        }
 
