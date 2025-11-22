fb408ddcb500752336cdd748681c0563597cf772
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 14:46:38 +0700

    Lab107

--- lab107.java
+++ lab107.java
@@ -3,14 +3,14 @@ import java.util.Scanner ;//เรียกใช้คำสั่ง Scanner
 public class lab107 {
 
     public static void main(String[] java){ 
-        Scanner entenNumber =new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
         System.out.print("Enter Number for sum number: "); 
-        int number =entenNumber.nextInt();
+        int number =input.nextInt();
         int i=0 ;
 
         for(int num = 1 ; num<=number ; num++ ){ 
         System.out.print("Enter Number : ");
-        int sum =entenNumber.nextInt();
+        int sum =input.nextInt();
         i+=sum; 
         }
         System.out.println("Answer = "+i);
