b291471a238ebede475f581c17b6901a46a49698
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 14:56:09 +0700

    Lab102

--- Lab102.java
+++ Lab102.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Lab102 {
-    public static void main(String[] args) { // main 
+    public static void main(String[] args) {  
 
     System.out.println("Enter is sum Number :");
-    Scanner Length =new Scanner(System.in);//รับค่าจากแป้นพิมพ์
-    Scanner Height =new Scanner(System.in);//รับค่าจากแป้นพิมพ์
+    Scanner Length =new Scanner(System.in);
+    Scanner Height =new Scanner(System.in);
 
     System.out.print("Enter Length : "); 
-    double L1 =Length.nextDouble();//นำค่า Length จากแป้นพิมพ์ มาใส่ในตัวแปร L1
+    double L1 =Length.nextDouble();
     System.out.print("Enter Height : "); 
-    double H1 =Height.nextDouble();//นำค่า Height จากแป้นพิมพ์ มาใส่ในตัวแปร H1
+    double H1 =Height.nextDouble();
     System.out.print("Answer =");
-    System.out.print(L1*H1);//แสดงผลลัพธ์
+    System.out.print(L1*H1);
   }
 }
 
