87e2ae227a4aa67107d54403a81f10c6e04be09a
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 12:07:24 +0700

    Lab108

--- Lab108.java
+++ Lab108.java
@@ -2,26 +2,25 @@ import java.util.Scanner;//เรียกใช้คำสั่ง Scanner
 
 public class Lab108 {
     public static void main(String[] nigger){
-    Scanner enterNumber =new Scanner(System.in); 
-    System.out.print("Enter Number :");
-    int number=enterNumber.nextInt();
-    int[] arryNumber = new int[number];
-    for(int i = 0 ;i < number ;i++ ){ 
-      arryNumber[i] =enterNumber.nextInt(); 
+       Scanner enterNumber =new Scanner(System.in); 
+       System.out.print("Enter Number :");
+       int number=enterNumber.nextInt();
+       int[] arryNumber = new int[number];
+       for(int i = 0 ;i < number ;i++ ){ 
+          arryNumber[i] =enterNumber.nextInt(); 
         
     }
-    int max;
-    max = 0;
-    for(int i = 0 ;i < number ; i++){
-       if(arryNumber[i] > max){
+    int max = 0;
+    
+      for(int i = 0 ;i < number ; i++){
+           if(arryNumber[i] > max){
 
-        max = arryNumber[i];
+           max = arryNumber[i];
 
        }
     }
     System.out.println(max);
-
-     
+    
     }
 }
 
