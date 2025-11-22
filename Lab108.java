1f12d4b80bd3d6f0a09e730b79dea9ba71b27d83
PanupongThongdee <pranupong.49@gmail.com>
2025-11-22 11:25:26 +0700

    push Lab

--- Lab108.java
+++ Lab108.java
@@ -5,9 +5,9 @@ public class Lab108 {
     Scanner enterNumber =new Scanner(System.in);    //สร้างตัวแปร enterNumber ไว้เก็บค่าจากคีย์บอร์ด
     System.out.print("Enter Number :");
     int number=enterNumber.nextInt(); //สร้างตัวแปร number ที่เป็นประเภท int ไว้เก็บค่าจาก entenNumber ที่รับมาจากแป้นพิมพ์
-    int[] arryNumber = new int[number];
-    for(int i = 0 ;i < number ;i++ ){
-      arryNumber[i] =enterNumber.nextInt();
+    int[] arryNumber = new int[number]; //สร้างตัวแปร arryNumber เป็น array และกำหนดความกว้างช่อง index ด้วยตัวแปร number 
+    for(int i = 0 ;i < number ;i++ ){ // loop i  ไว้กรอกตัวเลขใส่ในแต่ละindex
+      arryNumber[i] =enterNumber.nextInt();  //กรอกตัวเลขใส่ในแต่ละ index
         
     }
     int max = 0;
