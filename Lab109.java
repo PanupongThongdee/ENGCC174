c80753ec6cac45b66644aa030190c697b2dbc92d
PanupongThongdee <pranupong.49@gmail.com>
2025-11-23 15:42:09 +0700

    Lab109

--- Lab109.java
+++ Lab109.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class Lab109 {
     public static void main(String[] helloWorld){
-         Scanner EnterNum =new Scanner(System.in);
+         Scanner enterNum =new Scanner(System.in);
          System.out.print("Enter Number : ");
-         int N = EnterNum.nextInt();
+         int N = enterNum.nextInt();
          double[] index = new double[N];
          double sum = 0;
 
          for(int i = 0 ; i < N ; i++){
-         index[i] = EnterNum.nextDouble();
-         sum += index[i];
+             index[i] = enterNum.nextDouble();
+            sum += index[i];
          }
          
          System.out.println(sum/N);
-
+         enterNum.close();
+         
     }
 }
