c67d29d3e08656abd2b79d30b99dcaab07d98d8e
PanupongThongdee <pranupong.49@gmail.com>
2025-11-23 15:24:40 +0700

    Lab101

--- Lab101.java
+++ Lab101.java
@@ -4,14 +4,15 @@ public class Lab101 {
   public static void main(String[] args) { 
 
     System.out.println("Enter is sum Number :");
-    Scanner num1 =new Scanner(System.in);
-    Scanner num2 =new Scanner(System.in);
-
+    Scanner input =new Scanner(System.in);
+    
     System.out.print("Enter num1 : "); 
-    int n1 =num1.nextInt();
+    int n1 =input.nextInt();
     System.out.print("Enter num2 : "); 
-    int n2 =num2.nextInt();
+    int n2 =input.nextInt();
     System.out.print("Answer =");
     System.out.print(n1+n2);
+    input.close();
 }
+
 }
