de79581348ec7782be819ce8d6e8bd3134bfe107
PanupongThongdee <pranupong.49@gmail.com>
2025-12-01 22:25:50 +0700

    Lab3

---
+++ LAB3/Lab301.java
@@ -0,0 +1,11 @@
+package ENGCC174.LAB3;
+
+public class Lab301 {
+    
+    public static void main(String[] nigga){
+        for(int i = 0 ; i < 100 ; i++ ){
+            System.out.println("you are gay ");
+        }
+
+    }
+}
