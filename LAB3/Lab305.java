5d7f03be6b0f20165ebf4ecfbc3140e047cbc01f
PanupongThongdee <pranupong.49@gmail.com>
2025-12-04 10:45:17 +0700

    Lab3

--- LAB3/Lab305.java
+++ LAB3/Lab305.java
@@ -3,42 +3,45 @@ import java.util.Scanner ;
 public class Lab305 {
     public static void main (String[] gai){
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter connection : ");
         String Connection =input.nextLine();
+
         DatabaseConnection data = new DatabaseConnection(Connection);
         data.connect();
         data.disconnect();
         data.disconnect();
-       System.out.println(data.isConnected());
+        System.out.println(data.isConnected());
+        
     }
 }
 class DatabaseConnection {
     private String connectionString ;
     private boolean Connected ;
 
-    public DatabaseConnection(String connectinString){
-        this.connectionString = connectinString; 
-        this.Connected = false ;
+         public DatabaseConnection(String connectionString){
+             this.connectionString = connectionString; 
+             this.Connected = false ;
     }
-    public boolean isConnected(){
-        return Connected ;
+         public boolean isConnected(){
+             return Connected ;
     }
-    public void connect(){
-        if (Connected == false){
-            this.Connected = true ;
-            System.out.println("Connect to : " + connectionString);
+         public void connect(){
+              if (Connected == false){
+                 this.Connected = true ;
+                 System.out.println("Connect to : " + connectionString);
         }
-        else if(Connected == true){
-            System.out.println("Already connected. ");
+             else if(Connected == true){
+                System.out.println("Already connected. ");
         }
     }
-    public void disconnect(){
-        if(Connected == true){
-            this.Connected = false ;
-            System.out.println("Disconnected.");
+            public void disconnect(){
+                if(Connected == true){
+                  this.Connected = false ;
+                  System.out.println("Disconnected.");
         }
-        else if (Connected == false){
-            System.out.println("Already disconnected.");
+                else if (Connected == false){
+                  System.out.println("Already disconnected.");
         }
     } 
 }
