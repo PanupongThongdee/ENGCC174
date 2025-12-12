dcceb61162984600c94aee1f481f29323ae83307
PanupongThongdee <pranupong.49@gmail.com>
2025-12-12 13:31:54 +0700

    Lab4

---
+++ LAB4/lab401.java
@@ -0,0 +1,52 @@
+package LAB4;
+import java.util.Scanner;
+public class lab401 {
+    public static void main(String[] java){
+        Scanner data = new Scanner(System.in);
+        Book process = null ;
+        System.out.print("Enter number ");
+        int n = data.nextInt();
+
+        if(n == 1){
+            data.nextLine();
+            System.out.print("Enter Title :");
+            String title = data.nextLine();
+
+            process = new Book(title);
+            process.display();
+        }
+        else if(n == 2){
+            data.nextLine();
+            System.out.print("Enter Title :");
+            String title = data.nextLine();
+
+            System.out.print("Enter author :");
+            String author = data.nextLine();
+
+            process = new Book( title , author );
+            process.display();
+
+        }
+        
+        data.close();
+        process = null;
+    }
+}
+class Book {
+    private String title ;
+    private String author ;
+
+    public Book(String title){
+        this.title = title ;
+        this.author = "unknown" ;
+    }
+    public Book(String title , String author){
+        this.title = title ;
+        this.author = author ;
+    }
+    public void display(){
+        System.out.println("Title : " + title +"\n"+"Author : "+author) ;
+        
+    }
+}
+
